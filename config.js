23d312ba01c10824650dcf27019626612aff8d01
William Hendricks <wshendricks@gmail.comgit config --global user.email wshendricks@gmail.comgit config --global user.email wshendricks@gmail.com>
2018-12-11 12:30:57 -0600

    Set up user model, config.js, server.js. Configured a basic front end route. API routes next.

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'secret': 'oasdopjewkgklsjdgahgpewsh',
+  //var mydb = connect('host[:port]/mydb');
+  // //'database': mongodb://
+  'database': 'mongodb://localhost:27017/auth-db'
+  //maybe 'database': 'mongodb://localhost etc username pw
+}
